d3a19c5af1df90dd6065d0f693a46feca453793c
devstory111 <99546272+devstory111@users.noreply.github.com>
2022-03-25 16:06:06 +0400

    Delete css/js directory

--- css/js/popup.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".main").style.display = "block";
-        },
-        1000
-    )
-});
