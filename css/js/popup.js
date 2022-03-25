866ede60782e163c83fc3fcd150c2d69f4b73ca2
www.developers.today <contact@developers.today>
2022-03-25 11:55:54 +0400

    developers

---
+++ css/js/popup.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".main").style.display = "block";
+        },
+        1000
+    )
+});
